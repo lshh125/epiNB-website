88f9fd5af6f09e0e18b828bc824ec9b9eba8ce51
Shaoheng Liang <31879010+lshh125@users.noreply.github.com>
2022-10-19 15:18:33 -0500

    Update 6_🙋_About.py

--- pages/6_🙋_About.py
+++ pages/6_🙋_About.py
@@ -3,10 +3,11 @@ import streamlit as st
 st.title("About")
 
 st.markdown("## Authors")
-st.markdown("- Authors: Shaoheng Liang, Xianli Jiang, ..., Ken Chen")
-st.markdown("- Contact: sliang3@mdanderson.org")
-st.markdown("- Cite: manuscript under preparation")
-st.markdown("- Warranty: EpiNB is provided free of charge, and, therefore, on an \"as is\" basis, without warranty of any kind.")
+st.markdown("© 2022 Ken Chen lab. All rights reserved.")
+# st.markdown("- Authors: Shaoheng Liang, Xianli Jiang, ..., Ken Chen")
+# st.markdown("- Contact: sliang3@mdanderson.org")
+# st.markdown("- Cite: manuscript under preparation")
+# st.markdown("- Warranty: EpiNB is provided free of charge, and, therefore, on an \"as is\" basis, without warranty of any kind.")
 
 # st.markdown("## Downloadable version of epiNB")
 # st.markdown("- GitHub: https://github.com/KChen-lab")
