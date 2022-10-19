33a49b936348a9eb8780a2600421ba81e527dc43
Shaoheng Liang <31879010+lshh125@users.noreply.github.com>
2022-10-19 15:06:28 -0500

    Update 6_🙋_About.py

--- pages/6_🙋_About.py
+++ pages/6_🙋_About.py
@@ -8,9 +8,9 @@ st.markdown("- Contact: sliang3@mdanderson.org")
 st.markdown("- Cite: manuscript under preparation")
 st.markdown("- Warranty: EpiNB is provided free of charge, and, therefore, on an \"as is\" basis, without warranty of any kind.")
 
-st.markdown("## Downloadable version of epiNB")
-st.markdown("- GitHub: https://github.com/KChen-lab")
-st.markdown("- Documentation: https://epinb.readthedocs.io")
+# st.markdown("## Downloadable version of epiNB")
+# st.markdown("- GitHub: https://github.com/KChen-lab")
+# st.markdown("- Documentation: https://epinb.readthedocs.io")
 
 st.markdown("## Useful links")
 st.markdown("Our training data are curated from: http://hlathena.tools/")
